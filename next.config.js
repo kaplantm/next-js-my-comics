20e63f16953136ef020889bf0d23ea1e34911e43
Toni Kaplan <tkaplan@rightpoint.com>
2021-04-02 12:53:13 -0400

    next config trailingSlash

--- next.config.js
+++ next.config.js
@@ -4,4 +4,6 @@ module.exports = {
   generateBuildId: async () => {
     return time;
   },
+  // https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+  trailingSlash: true,
 };
