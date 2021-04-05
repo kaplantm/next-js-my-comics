788568f68526ed46ce8980cf74bce3a7227bf66c
Toni Kaplan <tonarie.k@gmail.com>
2021-04-04 22:54:51 -0400

    new build

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1c9d39ac5545bdbe48c5";
+/******/ 	var hotCurrentHash = "66f07d01a70289920d9d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
