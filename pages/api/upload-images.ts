faa491caf98885aa6af9938f65b058e783149751
Toni Kaplan <tonarie.k@gmail.com>
2021-04-11 16:32:24 -0400

    run prettier after image file upload

--- pages/api/upload-images.ts
+++ pages/api/upload-images.ts
@@ -1,5 +1,6 @@
 import AWS from "aws-sdk";
 import { promises } from "fs";
+import { exec } from "child_process";
 const { readFile, writeFile } = promises;
 
 AWS.config.credentials = {
@@ -10,8 +11,6 @@ AWS.config.credentials = {
 const bucketName = process.env.S3_URL.split("https://s3.amazonaws.com/")[1];
 
 const s3 = new AWS.S3({ apiVersion: "latest" });
-
-// TODO: handle panels pages and series pages edits
 
 export default async function handler(req, res) {
   const { paths, folder, jsonPath } = req.body;
@@ -24,15 +23,12 @@ export default async function handler(req, res) {
     return res.status(400).json("Paths array cannot be empty");
   }
 
-  // const imagePaths = paths.map((path) => `${folder}${path}`);
-
   const uploadedImages = [];
   try {
     await Promise.all(
       paths.map(async (imagePath) => {
         const pathInS3 = `${folder}/${imagePath.split("/").slice(-1)}`;
         const file = await readFile(`public${imagePath}`);
-        // TODO: add cache control metadata
         const objectParams = {
           Bucket: bucketName,
           Key: pathInS3,
@@ -59,6 +55,7 @@ export default async function handler(req, res) {
       imagesJsonFilePath,
       JSON.stringify([...imagesArrayFileData, ...uploadedImages])
     );
+    exec(`npm run prettier "${imagesJsonFilePath}"`, () => {});
 
     return res.status(200).json({
       filePaths: uploadedImages.map(
